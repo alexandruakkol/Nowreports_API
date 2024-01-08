e7e7d9c5f0788cdb6706c6ad3148c4f31201877d
alexandruakkol <alexandru.akkol@gmail.com>
2024-01-08 12:32:09 -0800

    mcap desc

--- DLfilings.js
+++ DLfilings.js
@@ -4,12 +4,14 @@ import {getDocNumber, sendErrorCIKToDB} from './server.js';
 async function startFilingsDownload(oo){
     let toDL_list;
     const sql = global.sqlconn;
-    if(oo.mode == 'update') toDL_list = (await sql.query(`select cik from companies`))?.recordset;
+    if(oo.mode == 'update') toDL_list = (await sql.query(`select cik from companies order by mcap desc`))?.recordset;
     if(oo.mode.startsWith('append') ) toDL_list = (await sql.query(`
         select c.cik 
         from companies c
         left join filings f on (f.cik=c.cik and f.year >= datepart(year, getdate())-1 )
-        where f.cik is null`))?.recordset;
+        where f.cik is null 
+        order by mcap desc
+        `))?.recordset;
         
     toDL_list = toDL_list.map(x=>x.cik);
 
@@ -25,7 +27,7 @@ async function startFilingsDownload(oo){
             await getDocNumber({cik, type:'annual'});
         }
         catch(err){
-            console.log(err)
+            console.log(err);
             sendErrorCIKToDB({cik, typ:'10-K'});
         }
         console.log('pulled ', cik);
