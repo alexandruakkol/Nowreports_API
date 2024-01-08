0c9b3213c56d4d191161b54e4fcdce731b778826
alexandruakkol <alexandru.akkol@gmail.com>
2024-01-08 08:56:44 -0800

    append fix

--- DLfilings.js
+++ DLfilings.js
@@ -5,11 +5,12 @@ async function startFilingsDownload(oo){
     let toDL_list;
     const sql = global.sqlconn;
     if(oo.mode == 'update') toDL_list = (await sql.query(`select cik from companies`))?.recordset;
-    if(oo.mode == 'update') toDL_list = (await sql.query(`
+    if(oo.mode == 'append') toDL_list = (await sql.query(`
         select c.cik 
         from companies c
         left join filings f on (f.cik=c.cik and f.year >= datepart(year, getdate())-1 )
         where f.cik is null`))?.recordset;
+
     toDL_list = toDL_list.map(x=>x.cik);
 
     console.log(`Pulling ${toDL_list.length} tickers`);
